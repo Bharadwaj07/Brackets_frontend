cf1e337cf7473e2429e7ae07f2df35e260efbc5b
Bharadwaj07 <yajamankr@gmail.com>
2021-02-20 13:54:59 +0530

    modified

--- src/app/shell/editor-header/editor-header.component.spec.ts
+++ src/app/shell/editor-header/editor-header.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { EditorHeaderComponent } from './editor-header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('EditorHeaderComponent', () => {
+  let component: EditorHeaderComponent;
+  let fixture: ComponentFixture<EditorHeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ EditorHeaderComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(EditorHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/shell/home-header/home-header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-header',
+  templateUrl: './home-header.component.html',
+  styleUrls: ['./home-header.component.css']
+})
+export class HomeHeaderComponent implements OnInit {
+
+  constructor(private route:Router) { }
+
+  ngOnInit(): void {
+  }
+  logout(){
+    localStorage.removeItem('authToken');
+    this.route.navigate(['login'])
+  }
+
+}
