f037aee6a4833d4f534f2636b7e31bd05151fd2c
Bharadwaj07 <yajamankr@gmail.com>
2021-02-20 13:53:06 +0530

    login modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { AuthService } from '../services/auth/auth.service';
 import {LoginService} from '../services/login.service';
 @Component({
   selector: 'app-login',
@@ -10,7 +12,11 @@ import {LoginService} from '../services/login.service';
 export class LoginComponent implements OnInit {
   logInForm:FormGroup;
   submitted:boolean = false;
-  constructor(private fb:FormBuilder,private userService:LoginService,private router:Router ) { }
+  constructor(private fb:FormBuilder,
+      private userService:LoginService,
+      private router:Router,
+      private auth :AuthService,
+      private _snackBar: MatSnackBar ) { }
 
   ngOnInit(): void {
    this.logInForm = this.fb.group({
@@ -23,11 +29,20 @@ export class LoginComponent implements OnInit {
   onSubmit(){
     if(this.logInForm.valid){
       this.userService.loginUser(this.logInForm.value).subscribe((data) =>{
-        const {token} = data;
-        localStorage.setItem('authToken',token);
-        // console.log(localStorage.getItem('auth'))
-        this.router.navigate(['home']);
-      } )
+        if(data.success){
+          if(this.auth.isAuthenticated()){
+            this.router.navigate(['home']);
+          }else{
+            console.log("invalid cred");
+          }
+        }else{
+          this._snackBar.open(data.message,"Invalid",{
+            duration:2000,
+          })
+        }
+      },(err:any) =>{
+        console.log(err)
+      })
     }
   }
 
